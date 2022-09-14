997a22fa297a125419f52a34362ecb7ccbd74580
ale05 <naomi.carcelen.jiron@utelvt.edu.ec>
2022-09-14 13:01:21 -0500

    creando un programa de suma

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+	float a,b,c;
+	cout<<"Porfavor ingrese 2 numeros"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"El resultado de la suma es:"<<c;
+	return 0;}
+
+
